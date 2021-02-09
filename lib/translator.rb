138c06e0e82e9f36e9139aba22f5441c124056c7
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 19:44:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
 
-def load_library
-  # code goes here
+require 'yaml'
+require 'pry'
+
+# load yml file
+# extract values from file
+# create key = emoticons
+# values =
+# english:
+# japanese:
+
+def load_library(file)
+  emoticons = YAML.load(file)
+  
+  binding.pry
 end
 
 def get_japanese_emoticon
