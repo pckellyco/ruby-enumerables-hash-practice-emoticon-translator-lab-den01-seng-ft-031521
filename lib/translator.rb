acd51730e2c3d255ee2d0b7ef4537eddf781e4cf
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 20:32:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ require 'pry'
 def load_library(file)
   emoticons = YAML.load_file(file)
   answer = emoticons.each_with_object({}) do |(emoticon, symbol), new_hash|
-    new_hash = {english: symbol[0], japanese: symbol[1]}
+    new_hash = {emoticon: {english: symbol[0], japanese: symbol[1]}}
     end
 end
 
