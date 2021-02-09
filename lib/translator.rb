7c29b364e9ca3db6aef4cf091cbfd0f5c12e9cdd
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 21:31:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,11 +30,12 @@ def get_japanese_emoticon(file, english_emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(file, english_emoticon)
+def get_english_meaning(file, japanese_emoticon)
   emotions = load_library(file)
   emotions.each do |emotion, language|
-    if emotions[emotion][:english] == english_emoticon
-        return emotions[emotion][:japanese]
+    if emotions[emotion][:japanese] == japanese_emoticon
+        return emotions[emotion][:english]
     end
   end
+  return "Sorry, that emoticon was not found"
 end
