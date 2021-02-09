c9f45896920675e37bd275a134254e638097bac4
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 21:13:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,14 +24,10 @@ def get_japanese_emoticon(file, english_emoticon)
   emoticon_library = load_library(file)
   emoticon_library.each do |emotion, languages|
     languages.each do |language, emoticon|
-      if languages[emoticon] == english_emoticon
-        
-  
-  symbols = emoticon_library.each do |key, value|
-    value.each do |language, symbol|
-      if value[language][symbol] === string
-      japanese_symbol = value[:japanese]
-      japanese_symbol
+      if languages[:english] == english_emoticon
+        languages[:japanese]
+      else "Sorry, that emoticon was not found"
+      end
     end
   end
 end
