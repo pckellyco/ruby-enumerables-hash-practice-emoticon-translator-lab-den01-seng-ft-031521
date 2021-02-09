2e907f0614860b36f0c8498f9eccb51858acaa70
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 20:17:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,11 +11,8 @@ require 'yaml'
 require 'pry'
 
 def load_library(file)
-  emoticons = YAML::parse(File.open(file))
-  new_hash = {}
-  emoticons do |key, value|
-    new_hash[key]
-  end
+  emoticons =File.open(file))
+  emoticons
 end
 
 def get_japanese_emoticon
