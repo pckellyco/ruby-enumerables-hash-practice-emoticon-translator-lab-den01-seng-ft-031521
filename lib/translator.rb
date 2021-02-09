a537b63652b05d9d5cea63cb33813f01efac81a9
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 20:56:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,7 @@ def get_japanese_emoticon(file, string)
     if emoticon_library[key][:english] == string
       japanese_symbol = emoticon_library[key][:japanese]
       japanese_symbol
+    else ""
       binding.pry
     end
   end
