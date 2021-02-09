f09a9427fd68c9d1e15a5c026919c18dc7ca6814
Paige Kelly <pckelly.co@gmail.com>
2021-02-09 20:25:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,9 @@ require 'pry'
 def load_library(file)
   emoticons = YAML.load_file(file)
   emoticons.each_with_object({}) do (|key, value|, new_hash)
-    binding.pry
+    value.each do |language|
+      binding.pry
+      end
     end
 end
 
